d06b64bdc47c95e0b71284bd71ef1bee6b6489bd
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2023-10-16 10:42:47 +0700

    feat: install iconify

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
+import { Icon } from "@iconify/react";
 import { Button } from "~/components/ui/button";
 
 import { api } from "~/utils/api";
@@ -16,14 +17,26 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        {isLoading ? (
+        {session == null ? (
+          <Button
+            variant="secondary"
+            onClick={() => void signIn("google")}
+            className="flex items-center gap-2 align-middle"
+          >
+            <Icon fontSize={24} icon="flat-color-icons:google" />
+            Sign In with Google
+          </Button>
+        ) : isLoading ? (
           <h1 className="text-white">Loading...</h1>
-        ) : session == null ? (
-          <Button onClick={() => void signIn("google")}>Sign In</Button>
         ) : (
           <>
-            <h1 className="text-white">{data?.greeting}</h1>
-            <Button onClick={() => void signOut()}>Sign Out</Button>
+            <h1 className="text-white">
+              {data?.greeting} And Hello {session.user.name}!
+            </h1>
+            <br />
+            <Button variant="secondary" onClick={() => void signOut()}>
+              Sign Out
+            </Button>
           </>
         )}
       </main>

6d9b71e70dccd29b625e3f41254db88bd45c0298
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2023-10-16 11:49:50 +0700

    feat: create initial home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,12 @@
 import { signIn, signOut, useSession } from "next-auth/react";
+import { LogOut, Loader2 } from "lucide-react";
 import Head from "next/head";
 import { Icon } from "@iconify/react";
 import { Button } from "~/components/ui/button";
-
-import { api } from "~/utils/api";
+import Link from "next/link";
 
 export default function Home() {
-  const { data, isLoading } = api.example.hello.useQuery({ text: "World!" });
-  const { data: session } = useSession();
+  const { status } = useSession();
 
   return (
     <>
@@ -16,29 +15,54 @@ export default function Home() {
         <meta name="description" content="Chat with your documents" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        {session == null ? (
-          <Button
-            variant="secondary"
-            onClick={() => void signIn("google")}
-            className="flex items-center gap-2 align-middle"
-          >
-            <Icon fontSize={24} icon="flat-color-icons:google" />
-            Sign In with Google
-          </Button>
-        ) : isLoading ? (
-          <h1 className="text-white">Loading...</h1>
-        ) : (
-          <>
-            <h1 className="text-white">
-              {data?.greeting} And Hello {session.user.name}!
-            </h1>
-            <br />
-            <Button variant="secondary" onClick={() => void signOut()}>
-              Sign Out
-            </Button>
-          </>
-        )}
+      <main className="flex min-h-screen w-screen flex-col items-center justify-center bg-gradient-to-b from-gray-200 to-blue-500">
+        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+          <div className="flex flex-col items-center text-center">
+            <div className="flex items-center">
+              <h1 className="text-5xl font-bold">Chat with any document</h1>
+            </div>
+
+            <p className="mt-3 max-w-xl text-lg text-slate-600">
+              Can&apos;t find what you need from a paper, presentation, PRD,
+              etc.? Chat with your document directly to find your answers ASAP.
+            </p>
+
+            <div className="mt-3 flex items-center gap-4">
+              {status === "unauthenticated" ? (
+                <Button
+                  className="flex flex-row justify-center gap-2 align-middle"
+                  onClick={() =>
+                    void signIn("google", { callbackUrl: "/chats" })
+                  }
+                >
+                  <Icon icon="flat-color-icons:google" fontSize={24} />
+                  Sign in with Google
+                </Button>
+              ) : status === "authenticated" ? (
+                <>
+                  <Button asChild variant="secondary">
+                    <Link href="/chats">Go to Chats</Link>
+                  </Button>
+                  <Button
+                    onClick={() => void signOut()}
+                    className="flex flex-row justify-center gap-2 align-middle"
+                  >
+                    <LogOut size={16} />
+                    Sign Out
+                  </Button>
+                </>
+              ) : (
+                <Button
+                  disabled
+                  className="flex flex-row justify-center gap-2 align-middle"
+                >
+                  <Loader2 size={16} />
+                  Loading
+                </Button>
+              )}
+            </div>
+          </div>
+        </div>
       </main>
     </>
   );
